6bdc7aec7b15ef7f02a3ad7bf6625c53a87d6865
edilson-santiago-da-slva <edilsonsantiagoe7@gmail.com>
2024-08-06 08:23:44 -0300

    login authentication part 3 final

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Credentials } from 'src/app/models/credentials';
 import { AuthService } from 'src/app/services/auth.service';
@@ -22,13 +23,15 @@ export class LoginComponent implements OnInit {
 
   constructor(
     private toast: ToastrService,
-    private service: AuthService) { }
+    private service: AuthService,
+    private router: Router) { }
 
   ngOnInit(): void {}
 
   logInto(){
     this.service.authenticate(this.creds).subscribe(response => {
       this.service.successfulLogin(response.headers.get('Authorization').substring(7))
+      this.router.navigate([''])
     }, () => {
       this.toast.error('Usuário e/ou senha inválidos')
     } )
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,11 +2,14 @@ import { Injectable } from '@angular/core';
 import { Credentials } from '../models/credentials';
 import { HttpClient } from '@angular/common/http';
 import { API_CONFIG } from '../config/api.config';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+
+  jwtService: JwtHelperService = new JwtHelperService();
 
   constructor(private http: HttpClient ) { }
 
@@ -20,4 +23,13 @@ export class AuthService {
   successfulLogin(authToken: string) {
     localStorage.setItem('token', authToken)
   }
+
+  isAuthenticated() {
+    let token = localStorage.getItem('token')
+    if(token != null) {
+      return !this.jwtService.isTokenExpired(token)
+    }
+    return false;
+  }
+
 }
