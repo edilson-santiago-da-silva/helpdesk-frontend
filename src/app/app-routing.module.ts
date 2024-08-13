eeaa3ee2c4f7e8be4aca1ee1ea9ae90763641a9b
edilson-santiago-da-slva <edilsonsantiagoe7@gmail.com>
2024-08-13 09:31:03 -0300

    route to TECHNICIAN-CREATE component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { HomeComponent } from './components/home/home.component';
 import { TechnicianListComponent } from './components/technician/technician-list/technician-list.component';
 import { LoginComponent } from './components/login/login.component';
 import { AuthGuard } from './auth/auth.guard';
+import { TechnicianCreateComponent } from './components/technician/technician-create/technician-create.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   {
     path: '', component: NavComponent, canActivate: [AuthGuard], children:[
       {path: 'home', component: HomeComponent},
-      {path: 'technician', component: TechnicianListComponent}
+      {path: 'technician', component: TechnicianListComponent},
+      {path: 'technician/create', component: TechnicianCreateComponent}
     ]
   }
 ];
