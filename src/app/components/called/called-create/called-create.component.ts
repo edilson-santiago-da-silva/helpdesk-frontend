e4f655c8db1877ae1d2c46671ff100e9e5eca1a4
edilson-santiago-da-slva <edilsonsantiagoe7@gmail.com>
2024-09-12 09:04:58 -0300

    Form validation for CALL-CREATE

--- src/app/components/called/called-create/called-create.component.ts
+++ src/app/components/called/called-create/called-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-called-create',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalledCreateComponent implements OnInit {
 
+  priority: FormControl = new FormControl(null, [Validators.required])
+  status: FormControl = new FormControl(null, [Validators.required])
+  title: FormControl = new FormControl(null, [Validators.required])
+  description: FormControl = new FormControl(null, [Validators.required])
+  technician: FormControl = new FormControl(null, [Validators.required])
+  client: FormControl = new FormControl(null, [Validators.required])
+
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
 }
