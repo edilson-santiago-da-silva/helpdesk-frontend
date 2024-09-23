ac6a1c3a7b0b8cad78f0027b194835f17468eb9d
edilson-santiago-da-slva <edilsonsantiagoe7@gmail.com>
2024-09-23 08:21:00 -0300

    CALLED-READ component finished

--- src/app/components/called/called-read/called-read.component.ts
+++ src/app/components/called/called-read/called-read.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Called } from 'src/app/models/called';
+import { CalledService } from 'src/app/services/called.service';
 
 @Component({
   selector: 'app-called-read',
@@ -6,10 +10,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./called-read.component.css']
 })
 export class CalledReadComponent implements OnInit {
+  called: Called ={
+    priority: '',
+    status: '',
+    title: '',
+    comments: '',
+    technician: '',
+    client: '',
+    nameClient: '',
+    nameTechnician: '',
+  }
 
-  constructor() { }
+  constructor(
+    private calledService: CalledService,
+    private toastService:    ToastrService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.called.id = this.route.snapshot.paramMap.get('id');
+    this.findById();
   }
+
+  findById(): void {
+    this.calledService.findById(this.called.id).subscribe(resposta => {
+      console.log(resposta);
+      this.called = resposta;
+    }, ex => {
+      this.toastService.error(ex.error.error);
+    })
+  }
+
+  returnStatus(status: any): string {
+    if(status == '0') {
+      return 'ABERTO'
+    } else if(status == '1') {
+      return 'EM ANDAMENTO'
+    } else {
+      return 'ENCERRADO'
+    }
+  }
+
+  returnPriority(priority: any): string {
+    if(priority == '0') {
+      return 'BAIXA'
+    } else if(priority == '1') {
+      return 'MÉDIA'
+    } else {
+      return 'ALTA'
+    }
+  }
+
 
 }
